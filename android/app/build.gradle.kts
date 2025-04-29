72ae69eac2e91a722903beaad04541aa07c9e227
tentamdin <tgtamdin3@gmail.com>
2025-04-29 21:22:59 +0530

    Update app name to Pecha and change bundle ID to org.pecha.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_pecha"
+    namespace = "org.pecha.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_pecha"
+        applicationId = "org.pecha.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
