e25fa1bfea85f4de422dfe7784ccc4b9fcbc5dc6
tentamdin <tgtamdin3@gmail.com>
2025-05-27 09:38:26 +0530

    feat: implement text details fetching and update segment model

---
+++ lib/features/texts/models/segment.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+
+class Segment {
+  final String segmentId;
+  final int segmentNumber;
+  final String content;
+  final String translation;
+
+  const Segment({
+    required this.segmentId,
+    required this.segmentNumber,
+    required this.content,
+    required this.translation,
+  });
+
+  factory Segment.fromJson(Map<String, dynamic> json) {
+    return Segment(
+      segmentId: json['segment_id'] as String,
+      segmentNumber: json['segment_number'] as int,
+      content: json['content'] as String,
+      translation: json['translation'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'segment_id': segmentId,
+      'segment_number': segmentNumber,
+      'content': content,
+      'translation': translation,
+    };
+  }
+}
