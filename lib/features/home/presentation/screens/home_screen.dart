fa5cb20e2ddae22a4253680c18e25d78708e8c63
TenzDelek <tibetdelek@gmail.com>
2026-06-23 12:47:49 +0530

    remove home calendar

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -15,7 +15,6 @@ import 'package:flutter_pecha/features/home/presentation/providers/series_provid
 import 'package:flutter_pecha/features/home/presentation/providers/verse_of_day_provider.dart';
 import 'package:flutter_pecha/features/home/presentation/home_screen_constants.dart';
 import 'package:flutter_pecha/features/home/presentation/widgets/featured_plan_section.dart';
-import 'package:flutter_pecha/features/home/presentation/widgets/home_calendar_card.dart';
 import 'package:flutter_pecha/features/home/presentation/widgets/home_header.dart';
 import 'package:flutter_pecha/features/home/presentation/widgets/home_share_prompt.dart';
 import 'package:flutter_pecha/features/home/presentation/widgets/home_shortcuts_row.dart';
@@ -342,7 +341,6 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          const HomeCalendarCard(),
                           const SizedBox(
                             height: HomeScreenConstants.cardSpacing,
                           ),
