647151ed769e677e16fe6d860dcb1ec290db7ec4
TenzDelek <tibetdelek@gmail.com>
2026-06-24 11:13:42 +0530

    changes

--- lib/features/practice/presentation/screens/all_recitations_screen.dart
+++ lib/features/practice/presentation/screens/all_recitations_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pecha/core/extensions/context_ext.dart';
+import 'package:flutter_pecha/features/practice/presentation/screens/recitations_search_screen.dart';
 import 'package:flutter_pecha/features/practice/presentation/widgets/practice_chant_list_tile.dart';
 import 'package:flutter_pecha/features/recitation/data/models/recitation_model.dart';
 
@@ -13,6 +14,12 @@ class AllRecitationsScreen extends StatelessWidget {
   final List<RecitationModel> recitations;
   final ValueChanged<RecitationModel> onTap;
 
+  void _openSearch(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => RecitationsSearchScreen(onTap: onTap)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
@@ -22,17 +29,24 @@ class AllRecitationsScreen extends StatelessWidget {
           l10n.home_chants,
           style: const TextStyle(fontWeight: FontWeight.bold),
         ),
-        centerTitle: false,
+        centerTitle: true,
         scrolledUnderElevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            tooltip: l10n.recitations_search_for,
+            onPressed: () => _openSearch(context),
+          ),
+        ],
       ),
       body: ListView.builder(
         padding: const EdgeInsets.only(top: 8, bottom: 16),
         itemCount: recitations.length,
         itemBuilder: (context, index) {
-          final r = recitations[index];
+          final recitation = recitations[index];
           return PracticeChantListTile(
-            recitation: r,
-            onTap: () => onTap(r),
+            recitation: recitation,
+            onTap: () => onTap(recitation),
           );
         },
       ),
